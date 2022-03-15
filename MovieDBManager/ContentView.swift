aa45041960247ae9a19deec3c6b79d3ca48ce195
Naciye Çelenli <naciye.celenli@ecodation.com>
2022-03-15 17:30:00 +0300

    Add login stack

--- MovieDBManager/ContentView.swift
+++ MovieDBManager/ContentView.swift
@@ -8,9 +8,61 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var email = ""
+    @State private var password = ""
+    @State private var showingLoginScreen =  true
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView{
+            ZStack {
+                Color.blue
+                    .ignoresSafeArea()
+                if(showingLoginScreen){
+                    VStack{
+                        Text("The Movie Manager")
+                            .font(.largeTitle)
+                            .foregroundColor(Color.white)
+                            .bold()
+                            .padding()
+                        Spacer()
+                        Text("Login with Email")
+                            .font(.title)
+                            .foregroundColor(Color.white)
+                            .bold()
+                            .padding()
+                        TextField("Email", text:$email)
+                            .padding()
+                            .frame(width: 300, height: 50)
+                            .background(Color.white)
+                            .cornerRadius(10)
+                        SecureField("Password",text: $password)
+                            .padding()
+                            .frame(width: 300, height: 50)
+                            .background(Color.white)
+                            .cornerRadius(10)
+                        Button("Login") {
+                            authUser(username: email, password: password)
+                        }
+                        .foregroundColor(.white)
+                        .frame(width: 300, height: 50)
+                        .background(Color.indigo)
+                        .cornerRadius(10)
+                        Spacer()
+                    }
+
+                }
+            
+                else{
+                    //Tabbar
+                }
+            }
+        }
+        .navigationBarHidden(true)
+    }
+    func authUser(username: String, password: String)
+    {
+        
+       showingLoginScreen = false
     }
 }
 
