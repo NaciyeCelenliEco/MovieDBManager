4843b7f7ce30db35271890be4a426223f333392b
Naciye Çelenli <naciye.celenli@ecodation.com>
2022-03-15 17:26:13 +0300

    Initial Commit

---
+++ MovieDBManager/MovieDBManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieDBManagerApp.swift
+//  MovieDBManager
+//
+//  Created by Naciye Celenli on 15.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct MovieDBManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
